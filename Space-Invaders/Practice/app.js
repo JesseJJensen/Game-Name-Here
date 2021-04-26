69ffc6693885ae4cf01e479ebe5b70b8ab3f0323
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-04-26 14:40:25 -0400

    added enemy ships

--- Space-Invaders/Practice/app.js
+++ Space-Invaders/Practice/app.js
@@ -1,3 +1,13 @@
-document.addeventListener("DOMContentLoaded", () => {
-    const squares = document.querySelectorAll('.grid div')
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const boxes = document.querySelectorAll('.grid div')
+    const resultDisplay = document.querySelector('#result')
+    let enemyIndex = 0
+
+
+    const enemy = [
+        0, 2, 4, 6, 8, 10, 12,
+        20, 22, 24, 26, 28, 30, 32 
+      ] 
+enemy.forEach( invader => boxes[enemyIndex + invader].classList.add('enemy'))
+
+    })
